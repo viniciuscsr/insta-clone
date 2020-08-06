0a634d43dba1773b70362a59374212f0635601c9
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-08-05 22:07:11 -0400

    comments controller

---
+++ controllers/commentsController.js
@@ -0,0 +1,62 @@
+const commentsController = {};
+
+const Post = require('../models/post');
+const User = require('../models/user');
+const Comment = require('../models/comment');
+const mongoose = require('mongoose');
+
+commentsController.postNewComment = async (req, res, next) => {
+  const newComment = new Comment({
+    text: req.body.text,
+    date: Date.now(),
+    user: {
+      id: req.user._id,
+      username: req.user.username,
+    },
+    post: req.params.postId,
+  });
+
+  let user;
+
+  try {
+    user = await User.findById(newComment.user.id);
+  } catch (err) {
+    return res.json({ message: err });
+  }
+
+  let post;
+
+  try {
+    post = await Post.findById(newComment.post);
+  } catch (err) {
+    return res.json({ message: err });
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newComment.save({ session: session });
+    user.comments.push(newComment);
+    await user.save({ session: session });
+    post.comments.push(newComment);
+    await post.save({ session: session });
+    await session.commitTransaction();
+  } catch (err) {
+    await session.abortTransaction();
+    console.error(err);
+    throw err;
+  } finally {
+    res.redirect('/posts/' + newComment.post);
+  }
+};
+
+commentsController.deleteComment = async (req, res) => {
+  try {
+    await Comment.remove({ _id: req.params.commentId });
+  } catch (error) {
+    res.json({ message: 'Could not delete the comment' });
+  }
+  res.redirect('/posts/' + req.params.postId);
+};
+
+module.exports = commentsController;
--- routes/comments.js
+++ routes/comments.js
@@ -1,75 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const router = express.Router({ mergeParams: true });
 
-const Comment = require('../models/comment');
 const middleware = require('../middleware/index');
-const Post = require('../models/post');
-const User = require('../models/user');
+const commentsController = require('../controllers/commentsController');
 
 // POST A NEW COMMENT
 
-router.post('/', middleware.isLoggedIn, async (req, res, next) => {
-  const newComment = new Comment({
-    text: req.body.text,
-    date: Date.now(),
-    user: {
-      id: req.user._id,
-      username: req.user.username,
-    },
-    post: req.params.postId,
-  });
-
-  let user;
-
-  try {
-    user = await User.findById(newComment.user.id);
-  } catch (err) {
-    return res.json({ message: err });
-  }
-
-  let post;
-
-  try {
-    post = await Post.findById(newComment.post);
-  } catch (err) {
-    return res.json({ message: err });
-  }
-
-  // try {
-  //   await newComment.save();
-  // } catch (error) {
-  //   res.json({ message: 'Something went wrong' });
-  // }
-
-  try {
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    await newComment.save({ session: session });
-    user.comments.push(newComment);
-    await user.save({ session: session });
-    post.comments.push(newComment);
-    await post.save({ session: session });
-    await session.commitTransaction();
-  } catch (err) {
-    await session.abortTransaction();
-    console.error(err);
-    throw err;
-  } finally {
-    res.redirect('/posts/' + newComment.post);
-  }
-});
+router.post('/', middleware.isLoggedIn, commentsController.postNewComment);
 
 // DELETE A COMMENT
 
-router.delete('/:commentId', middleware.commentOwnership, async (req, res) => {
-  try {
-    await Comment.remove({ _id: req.params.commentId });
-  } catch (error) {
-    res.json({ message: 'Could not delete the comment' });
-  }
-  res.redirect('/posts/' + req.params.postId);
-});
+router.delete(
+  '/:commentId',
+  middleware.commentOwnership,
+  commentsController.deleteComment
+);
 
 module.exports = router;
